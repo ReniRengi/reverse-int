4c4892f4969505b184ab87bb5027eaccd416bfbf
Olga Bogdanova <TomieForever@gmail.com>
2021-08-12 17:57:21 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function reverse (n) {
-  
-}
+    let newNumber=[];
+    if(n<0){
+      n= Math.abs(n);
+      while(n>0){
+    newNumber.push(n%10);
+    n=Math.floor(n/10);
+    }
+    }
+    else{ 
+    while(n>0){
+    newNumber.push(n%10);
+    n=Math.floor(n/10);
+    }
+    }
+    newNumber=newNumber.join("");
+    newNumber=Number.parseInt(newNumber);
+    return newNumber;
+ }
+
